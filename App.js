d0992ccb48eccd1f77ffbc76b9cb1a43a7ac80e8
Anthony Elliott <aelliott@pillartechnology.com>
2018-06-02 15:38:01 -0500

    Add biscuits and gravy to first page

--- App.js
+++ App.js
@@ -6,18 +6,10 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -25,13 +17,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container} testID="recipeList">
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+          Biscuits and Gravy
         </Text>
       </View>
     );
