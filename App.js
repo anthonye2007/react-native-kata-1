9a6926dd6712bfc88e4ae24a8b2984c83efa9310
Anthony Elliott <aelliott@pillartechnology.com>
2018-06-03 15:08:42 -0500

    Spike for navigation

--- App.js
+++ App.js
@@ -6,17 +6,31 @@
 
 import React, { Component } from 'react';
 import {
-  FlatList, Text
+  View, FlatList, Text
 } from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+});
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <FlatList testID="recipeList"
-        data={[{title: 'Biscuits and Gravy'}, {title: 'Tacos'}]}
-        renderItem={({item}) => <Text>{item.title}</Text>}
-      />
+      <RootStack/>
     );
   }
 }
