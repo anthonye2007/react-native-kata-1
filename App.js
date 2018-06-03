d89071e401f43b8afd69f6c67fc412afc5ac4090
Anthony Elliott <aelliott@pillartechnology.com>
2018-06-03 15:11:57 -0500

    Add two routes

--- App.js
+++ App.js
@@ -20,11 +20,24 @@ class HomeScreen extends React.Component {
   }
 }
 
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
 const RootStack = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  },
-});
+  Home: HomeScreen,
+  Details: DetailsScreen,
+},
+  {
+    initialRouteName: 'Home'
+  }
+);
 
 type Props = {};
 export default class App extends Component<Props> {
