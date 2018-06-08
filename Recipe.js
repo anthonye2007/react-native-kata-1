7d55ddaf12b22e2e6d0b8b31d1e0e1504540884a
Anthony Elliott <aelliott@pillartechnology.com>
2018-06-08 06:22:53 -0500

    Add rating

--- Recipe.js
+++ Recipe.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 export default function Recipe(props) {
-  return <Text>{props.recipeName}</Text>;
+  return (
+    <View>
+      <Text>{props.recipeName}</Text>
+      <Text>{props.rating} stars</Text>
+    </View>
+  );
 }
--- test/recipe.spec.js
+++ test/recipe.spec.js
@@ -1,10 +1,16 @@
 import {shallow} from 'enzyme';
 import React from 'react';
 import Recipe from '../Recipe';
+import {getAllText} from './helpers';
 
 describe('Recipe', () => {
   it('should have a recipe name', () => {
     let recipe = shallow(<Recipe recipeName='foobar'/>);
-    expect(recipe.dive().text()).toContain('foobar');
+    expect(getAllText(recipe)).toContain('foobar');
+  });
+
+  it('should have a rating', () => {
+    let recipe = shallow(<Recipe recipeName='foobar' rating='5'/>);
+    expect(getAllText(recipe)).toContain('5 stars');
   });
 });
